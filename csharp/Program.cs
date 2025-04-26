df879defe2956119c1663bed7e1c602cfc047f28
marrt <marrttaoburberry@gmail.com>
2025-04-26 16:47:10 +0300

    completed task 1

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,9 +1,75 @@
 ﻿namespace csharp;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    private static Random rand = new Random();
+
+    public int[] GetRandomArray(int size)
     {
-        Console.WriteLine("Hello, World!");
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = rand.Next(1, 100);
+        }
+        return arr;
+    }
+
+    public int UniqueElements(int[] array)
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            bool isUnique = true;
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (i != j && array[i] == array[j])
+                {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int EvenElements(int[] array)
+    {
+        int count = 0;
+        foreach (int num in array)
+        {
+            if (num % 2 == 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int OddElements(int[] array)
+    {
+        int count = 0;
+        foreach (int num in array)
+        {
+            if (num % 2 != 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void Main(string[] args)
+    {
+        Program program = new Program();
+        int[] randomArray = program.GetRandomArray(10);
+
+        Console.WriteLine("Random Array: " + string.Join(", ", randomArray));
+        Console.WriteLine("Unique Elements: " + program.UniqueElements(randomArray));
+        Console.WriteLine("Even Elements: " + program.EvenElements(randomArray));
+        Console.WriteLine("Odd Elements: " + program.OddElements(randomArray));
     }
 }
---
+++ tests/Test1.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using csharp;
+using System.Linq;
+
+namespace csharpTests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        private Program _program;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _program = new Program();
+        }
+
+        [TestMethod]
+        public void UniqueElements_AllDifferent_ReturnsArrayLength()
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            int result = _program.UniqueElements(array);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void UniqueElements_AllSame_ReturnsZero()
+        {
+            int[] array = { 7, 7, 7, 7, 7 };
+            int result = _program.UniqueElements(array);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void UniqueElements_MixedUniqueAndDuplicates_ReturnsCorrectCount()
+        {
+            int[] array = { 1, 2, 2, 3, 4, 4, 5 };
+            int result = _program.UniqueElements(array);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void EvenElements_CorrectCount()
+        {
+            int[] array = { 2, 4, 5, 7, 8 };
+            int result = _program.EvenElements(array);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void OddElements_CorrectCount()
+        {
+            int[] array = { 2, 4, 5, 7, 8 };
+            int result = _program.OddElements(array);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void GetRandomArray_CorrectSize()
+        {
+            int size = 10;
+            int[] result = _program.GetRandomArray(size);
+            Assert.AreEqual(size, result.Length);
+        }
+
+        [TestMethod]
+        public void GetRandomArray_ElementsInRange()
+        {
+            int size = 20;
+            int[] result = _program.GetRandomArray(size);
+            bool allInRange = result.All(x => x >= 1 && x < 100);
+            Assert.IsTrue(allInRange);
+        }
+    }
+}
